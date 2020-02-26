4df8a7fae4baabbd5a10e0eff71395cd165f2ea9
蔡昀辰 Cheney.Tsai <Cheney.Tsai@jet-opto.com.tw>
2020-02-26 16:09:46 +0800

    can run shell

--- main.ts
+++ main.ts
@@ -1,18 +1,44 @@
-const readline = require('readline');
-
+  
+var process = require('child_process');
+var exec = require('child_process').exec;
+var iconv = require('iconv-lite');
+var  readline = require('readline');
+var gpg = require('gpg')
+var fileDecrypted ='./testdecrypt/de.txt'
+var fileEncrypted = './testdecrypt/en.txt'
+/*
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
+});*/
+exec(` gpg --batch --passphrase-fd 0 --armor --decrypt ./testdecrypt/en.txt < pwd.txt`, function(error, stdout, stderr){
+    if(error) {
+        console.error('error: ' + error,'cp950');
+        console.error('error: '+error)
+        return;
+    }
+   
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' +  stderr);
 });
-
 /*
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+exec(`gpg --output ./testdecrypt/de.txt  --decrypt ./testdecrypt/en.txt`,{encoding:'binaryEncoding'}, function(error, stdout, stderr){
+    if(error) {
+        //console.error('error: ' + iconv.decode(error,'cp950'));
+        //console.error('error: '+error)
+        return;
+    }
+   
+    console.log('stdout: ' + iconv.decode(stdout,'cp950'));
+    console.log('stderr: ' +  stderr);
 });*/
 
-rl.write('Delete this!');
-// Simulate Ctrl+u to delete the line written previously
-rl.write(null, { ctrl: true, name: 'u' });
+/*
+gpg.callStreaming(fileEncrypted, fileDecrypted, 'fuck', (err, success) => {
+    // success/err
+    if(err)
+        console.log(err)
+    else if(success)
+        console.log(success)
+
+  });*/
---
+++ stdinread.ts
@@ -0,0 +1,6 @@
+var test= function(d){
+    process.stdout.write(d+'\n');
+}
+
+
+test('fuck')
