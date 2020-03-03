a42bcce5cfcbb1073a13774c6e373a7996a00e07
蔡昀辰 Cheney.Tsai <Cheney.Tsai@jet-opto.com.tw>
2020-03-03 18:30:44 +0800

    add record last testing into json file

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ var exec = require('child_process').exec;
 var order =[null,"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
 "u","v","w","x","y","z"]
 
+
+/*
+order.forEach(element => {
+    console.log(`"${element}" : "[0,0,0,0]",`)
+});
+*/
+
+
 for(var i=1;i<order.length;i+=2){
   exec(`start cmd /k "node power.ts ${order[i]} ${order[i+1]} "` , function(error, stdout, stderr){
     if(error) {
--- power.ts
+++ power.ts
@@ -2,7 +2,7 @@
 var fs = require('fs')
 var exec = require('child_process').exec;
 var spawn = require("child_process").spawn,child;
-var orderstring ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//var orderstring ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 //var order =[null,"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
 //"u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
 var order =[null,"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l",
@@ -154,6 +154,10 @@ function execShellCommand(input,prefix) {
         {
             console.log(`prefix : ${prefix1} ${prefix2} test to ${init} ${tostring(init)}\n`)
             fs.appendFileSync('record',`prefix : ${prefix1} ${prefix2} test to ${init} ${tostring(init)}\n`)
+            var lasttest = JSON.parse(fs.readFileSync('record.json'))
+            lasttest[prefix1] = init
+            lasttest[prefix2] = init
+            fs.writeFileSync('record.json',JSON.stringify(lasttest))
         }
     }
 
